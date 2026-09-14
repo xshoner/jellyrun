c49facbbf7b7398e2f7cbe6966758dd995fe4dc2
xshoner <xshoner@gmail.com>
2026-09-14 19:11:36 +0900

    Reduce pirate fire immediate damage to thirty percent

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = 'jellyrun-v16';
-const CORE = ['./', './index.html', './style.css?v=16', './game.js?v=16', './audio.js?v=16', './leaderboard.js?v=16', './pwa.js?v=16', './manifest.webmanifest'];
+const CACHE = 'jellyrun-v17';
+const CORE = ['./', './index.html', './style.css?v=17', './game.js?v=17', './audio.js?v=17', './leaderboard.js?v=17', './pwa.js?v=17', './manifest.webmanifest'];
 self.addEventListener('install', event => event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(CORE)).then(() => self.skipWaiting())));
 self.addEventListener('activate', event => event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE).map(key => caches.delete(key)))).then(() => self.clients.claim())));
 self.addEventListener('fetch', event => {
